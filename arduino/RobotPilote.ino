ffd1558c329c9432c3452916f8b2991e6fd116e8
Emmanuel Lecomte <emmanuel.lecomte@zenika.com>
2018-12-12 09:56:23 +0100

    :construction: Improved sequential processing workflow

--- arduino/RobotPilote.ino
+++ arduino/RobotPilote.ino
@@ -1,64 +1,76 @@
-#include <SR04.h>
 #include <LedControl.h>
 #include <Adafruit_NeoPixel.h>
 #include <Wire.h>
 #include <pitches.h>
+#include <MPU6050_tockn_fork.h>
 
 // Left HS sensor
-#define HC_LEFT_ECHO_PIN 5
-#define HC_LEFT_TRIG_PIN 4
+#define HC_LEFT_ECHO_PIN    5
+#define HC_LEFT_TRIG_PIN    4
 
 // Right HS sensor
-#define HC_RIGHT_ECHO_PIN 3
-#define HC_RIGHT_TRIG_PIN 2
+#define HC_RIGHT_ECHO_PIN   3
+#define HC_RIGHT_TRIG_PIN   2
 
 // Beeper
-#define BUZZER_PIN 7
+#define BUZZER_PIN          7
 
 // LED Matrix
-#define LED_MRX_DATA_IN 12
-#define LED_MRX_CS 11
-#define LED_MRX_CLK 10
+#define LED_MRX_DATA_IN     12
+#define LED_MRX_CS          11
+#define LED_MRX_CLK         10
 
 // Motor driver 
-#define LEFT_DIR_PIN   32
-#define LEFT_DRV_PIN   8 // Need PWM
-#define RIGHT_DIR_PIN  33
-#define RIGHT_DRV_PIN  9 // Need PWM
+#define LEFT_DIR_PIN        32
+#define LEFT_DRV_PIN        8 // Need PWM
+#define RIGHT_DIR_PIN       33
+#define RIGHT_DRV_PIN       9 // Need PWM
 
 // RGB band
-#define RGB_PIN 6
+#define RGB_PIN             6
+#define RGB_LED_COUNT       15
 
 // Status LED
-#define LED_STARTED 31
+#define LED_STARTED         31
 
 // Various delay
-int monitoringDelay = 200;
-int actionDelay = 500;
-int shortestDelay = 50;
+#define MINIMAL_DELAY       25
+#define MONITORING_DELAY    20 * MINIMAL_DELAY
+#define SHORT_ACTION_DELAY  10 * MINIMAL_DELAY
+#define ACTION_DELEAY       20 * MINIMAL_DELAY
 
 // And loop control
 int monitoringStep = 0;
 int actionStep = 0;
+int shortActionStep = 0;
 
 void setup() {  
 
+  pinMode(LED_STARTED, OUTPUT);
+  digitalWrite(LED_STARTED, HIGH);
+  
   // Start sensors / actions
-  startAccel();
   startFace();
+  displayLoadingFace(1);
+  delay(500);
   startSerial();
+  pinMode(BUZZER_PIN, OUTPUT);
+  startAccel();
   startBand();
   setupMotors();
+  startHsSensors();
+  displayLoadingFace(2);
+  delay(500);
+  zeroAccel();
+  displayLoadingFace(3);
   
   // Do a beep
   doBeep();
+  setFaceAction('1');
   
-  // Startup led
-  pinMode(LED_STARTED, OUTPUT);
-  digitalWrite(LED_STARTED, HIGH);   // turn the LED on (HIGH is the voltage level)
-
   // Ready (TODO : raspb ACK)
   sendReady();
+  digitalWrite(LED_STARTED, LOW);
 }
 
 void loop() {
@@ -67,24 +79,33 @@ void loop() {
     processCommand();
   }
   
-  if(monitoringStep >= monitoringDelay){
+  if(monitoringStep >= MONITORING_DELAY){
+    updateDistances();
+    updateAccel();
     doMonitoring();
     monitoringStep = 0;
   } else {
-    monitoringStep += shortestDelay;
+    monitoringStep += MINIMAL_DELAY;
   }
   
-  if(actionStep >= actionDelay){
+  if(actionStep >= ACTION_DELEAY){
     doBandAction();
     doFaceAction();
+    doBuzzerAction();
     actionStep = 0;
   } else {
-    actionStep += shortestDelay;
+    actionStep += MINIMAL_DELAY;
   }
 
-  updateAccel();
-  updateDistances();
-  doBandShortAction();
-
-  delay(shortestDelay);
+  if(actionStep >= SHORT_ACTION_DELAY){
+    doBandShortAction();
+    doBuzzerShortAction();
+    shortActionStep = 0;
+  } else {
+    shortActionStep += MINIMAL_DELAY;
+  }
+  
+  delay(MINIMAL_DELAY);
+  digitalWrite(LED_STARTED, LOW);
 }
+
